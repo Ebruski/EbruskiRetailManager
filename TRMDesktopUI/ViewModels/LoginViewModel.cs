7d42c95f9189ff5feb47b4ce28ae60cea5b793bd
Obigba Ebruski <o.ebruski@gmail.com>
2021-01-29 23:21:34 +0100

    Login Form working Properly

--- TRMDesktopUI/ViewModels/LoginViewModel.cs
+++ TRMDesktopUI/ViewModels/LoginViewModel.cs
@@ -12,6 +12,8 @@ namespace TRMDesktopUI.ViewModels
     {
         private string _userName;
         private string _password;
+        private string _errorMessage;
+
         private IAPIHelper _apiHelper;
 
         public string UserName
@@ -49,6 +51,32 @@ namespace TRMDesktopUI.ViewModels
             }
         }
 
+        public bool IsErrorVisible
+        {
+            get 
+            {
+                bool output = false;
+
+                if (ErrorMessage?.Length > 0)
+                {
+                    output = true;
+                }
+
+                return output;
+            }
+        }
+
+        public string ErrorMessage
+        {
+            get { return _errorMessage; }
+            set
+            {
+                _errorMessage = value;
+                NotifyOfPropertyChange(() => IsErrorVisible);
+                NotifyOfPropertyChange(() => ErrorMessage);
+            }
+        }
+
         public LoginViewModel(IAPIHelper aPIHelper)
         {
             _apiHelper = aPIHelper;
@@ -58,11 +86,13 @@ namespace TRMDesktopUI.ViewModels
         {
             try
             {
+                ErrorMessage = "";
+
                 var result = await _apiHelper.Authenticate(UserName, Password);
             }
             catch (Exception ex)
             {
-                Console.WriteLine(ex.Message);
+                ErrorMessage = ex.Message;
             }
         }
     }
