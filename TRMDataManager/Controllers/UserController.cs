994666f7378ec2b360688ebd80ad93ffd800d563
Obigba Ebruski <o.ebruski@gmail.com>
2021-01-30 01:16:35 +0100

    Added DataAccess

---
+++ TRMDataManager/Controllers/UserController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using TRMDataManager.Library.DataAccess;
+using TRMDataManager.Library.Models;
+using Microsoft.AspNet.Identity;
+
+namespace TRMDataManager.Controllers
+{
+    [Authorize]
+    [RoutePrefix("api/User")]
+    public class UserController : ApiController
+    {
+        public List<UserModel> GetById()
+        {
+            var userId = RequestContext.Principal.Identity.GetUserId();
+
+            UserData data = new UserData();
+
+            return data.GetUserId(userId);
+        }
+    }
+}
