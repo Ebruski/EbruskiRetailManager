e52777e3bd4ad6dd7aabbb5488a5fa047f39f37e
Obigba Ebruski <o.ebruski@gmail.com>
2021-02-01 02:34:31 +0100

    Add Foreign Key Relationships

--- TRMData/dbo/Tables/SaleDetails.sql
+++ TRMData/dbo/Tables/SaleDetails.sql
@@ -5,6 +5,8 @@
 	[ProductId] INT NOT NULL,
 	[Quantuty] INT NOT NULL DEFAULT 1,
 	[PurchasePrice] MONEY NOT NULL, 
-	[Tax] MONEY NOT NULL DEFAULT 0
+	[Tax] MONEY NOT NULL DEFAULT 0, CONSTRAINT [FK_SaleDetails_ToSale] FOREIGN KEY (SaleId) REFERENCES Sale(Id), 
+	CONSTRAINT [FK_SaleDetails_ToProduct] FOREIGN KEY (ProductId) REFERENCES Product(Id)
+	
 	
 )
